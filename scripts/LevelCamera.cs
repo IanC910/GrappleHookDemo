5b91b64a95b295de57166bf4be5156481b4276a0
Ian <thecruik@gmail.com>
2025-06-02 23:47:36 -0700

    Initial Commit

---
+++ scripts/LevelCamera.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class LevelCamera : Camera2D {
+    RigidBody2D player = null;
+
+    [Export] int cameraHeightOffset = 400;
+
+    public override void _Ready()
+    {
+        player = GetNode<RigidBody2D>("../Player");
+
+        this.Zoom = new Vector2(0.5f, 0.5f);
+    }
+
+    public override void _Process(double delta)
+    {
+        this.Rotation = player.Rotation;
+        this.Position = new Vector2(player.Position.X, player.Position.Y - cameraHeightOffset);
+    }
+}
